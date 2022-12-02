b9be1e791185c407095134740f47d1c420540fce
Joel <joelart05@yahoo.com>
2022-12-01 17:18:07 -0800

    Initial Commit

---
+++ 2/_App.swift
@@ -0,0 +1,17 @@
+//
+//  _App.swift
+//  2
+//
+//  Created by Joel Arthurs on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct _App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
